fb2194cd48be0333959ca917a5a9e79bf4e0ffaa
Javy145 <39290955+Javy145@users.noreply.github.com>
2018-06-17 20:51:21 -0600

    Add files via upload

---
+++ Animales Silvestres/Animales_Silvestres.js
@@ -0,0 +1,17 @@
+$(function() {
+    var selectedClass = "";
+    $(".filter").click(function(){
+        selectedClass = $(this).attr("data-rel");
+        $("#gallery").fadeTo(100, 0.1);
+        $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+        setTimeout(function() {
+            $("."+selectedClass).fadeIn().addClass('animation');
+            $("#gallery").fadeTo(300, 1);
+        }, 300);
+    });
+});
+
+
+var shiftWindow = function() { scrollBy(0, -50) };
+if (location.hash) shiftWindow();
+window.addEventListener("hashchange", shiftWindow);
